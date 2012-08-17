acf1c1ec4628521316c7e1d2a0c9bc03e67e40dd
develar <develar@gmail.com>
2012-08-17 16:43:13 +0400

    cosmetics
    (cherry-picked from bcd622a)

--- js/js.translator/src/org/jetbrains/k2js/translate/expression/TryTranslator.java
+++ js/js.translator/src/org/jetbrains/k2js/translate/expression/TryTranslator.java
@@ -16,11 +16,11 @@
 
 package org.jetbrains.k2js.translate.expression;
 
-import com.google.common.collect.Lists;
 import com.google.dart.compiler.backend.js.ast.JsBlock;
 import com.google.dart.compiler.backend.js.ast.JsCatch;
 import com.google.dart.compiler.backend.js.ast.JsName;
 import com.google.dart.compiler.backend.js.ast.JsTry;
+import com.intellij.util.SmartList;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.jet.lang.psi.*;
@@ -73,7 +73,7 @@ public final class TryTranslator extends AbstractTranslator {
 
     @NotNull
     private List<JsCatch> translateCatches() {
-        List<JsCatch> result = Lists.newArrayList();
+        List<JsCatch> result = new SmartList<JsCatch>();
         for (JetCatchClause catchClause : expression.getCatchClauses()) {
             result.add(translateCatchClause(catchClause));
         }
