53de00297b25405bcab0a49aae4ccec48758690d
develar <develar@gmail.com>
2012-09-06 18:30:58 +0400

    objectAsConstructorParameter
    (cherry-picked from 41cb0ab)

--- js/js.tests/test/org/jetbrains/k2js/test/semantics/ClosureTest.java
+++ js/js.tests/test/org/jetbrains/k2js/test/semantics/ClosureTest.java
@@ -42,4 +42,8 @@ public final class ClosureTest extends SingleFileTranslationTest {
     public void testLocalParameterInLocalNamedFunction() throws Exception {
         fooBoxTest();
     }
+
+    public void testObjectAsConstructorParameter() throws Exception {
+        fooBoxTest();
+    }
 }
