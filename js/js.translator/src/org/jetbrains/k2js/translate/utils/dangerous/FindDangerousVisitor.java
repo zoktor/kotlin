189ef61e45d9b67fb0ecb406bb2a03d71eb0c980
develar <develar@gmail.com>
2012-09-06 20:20:51 +0400

    revert KT-2205
    (cherry-picked from 8cd2c07)

--- js/js.translator/src/org/jetbrains/k2js/translate/utils/dangerous/FindDangerousVisitor.java
+++ js/js.translator/src/org/jetbrains/k2js/translate/utils/dangerous/FindDangerousVisitor.java
@@ -57,6 +57,14 @@ public final class FindDangerousVisitor extends JetTreeVisitor<DangerousData> {
     }
 
     @Override
+    public Void visitIfExpression(JetIfExpression expression, DangerousData data) {
+        if (expressionFound(expression, data)) {
+            return null;
+        }
+        return super.visitIfExpression(expression, data);
+    }
+
+    @Override
     public Void visitBlockExpression(JetBlockExpression expression, DangerousData data) {
         if (isStatement(context.bindingContext(), expression)) {
             return null;
