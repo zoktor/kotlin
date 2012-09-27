3df878ba57df7e9159e70e2b0f17f225ae50569f
develar <develar@gmail.com>
2012-09-27 17:36:05 +0400

    fix Map set/get — should work with jet Map/MutableMap
    (cherry-picked from de7db26)

--- js/js.libraries/src/core/javautil.kt
+++ js/js.libraries/src/core/javautil.kt
@@ -3,7 +3,7 @@ package java.util
 import java.lang.*
 
 library("collectionsMax")
-public fun max<T>(col : Collection<T>, comp : Comparator<T>) : T = js.noImpl
+public fun max<T>(col : jet.Collection<T>, comp : Comparator<T>) : T = js.noImpl
 
 library
 public trait Comparator<T> {
@@ -22,10 +22,10 @@ public abstract class AbstractCollection<E>() : MutableCollection<E> {
     override fun add(e: E): Boolean = js.noImpl
     override fun remove(o: Any?): Boolean = js.noImpl
 
-    override fun addAll(c: Collection<out E>): Boolean = js.noImpl
-    override fun containsAll(c : Collection<out Any?>) : Boolean = js.noImpl
-    override fun removeAll(c : Collection<out Any?>) : Boolean = js.noImpl
-    override fun retainAll(c : Collection<out Any?>) : Boolean = js.noImpl
+    override fun addAll(c: jet.Collection<out E>): Boolean = js.noImpl
+    override fun containsAll(c : jet.Collection<out Any?>) : Boolean = js.noImpl
+    override fun removeAll(c : jet.Collection<out Any?>) : Boolean = js.noImpl
+    override fun retainAll(c : jet.Collection<out Any?>) : Boolean = js.noImpl
 
     override fun clear(): Unit = js.noImpl
     override fun size(): Int = js.noImpl
@@ -40,7 +40,7 @@ native public abstract class AbstractList<E>(): AbstractCollection<E>(), Mutable
 
     override fun add(e: E): Boolean = js.noImpl
     override fun add(index: Int, element: E): Unit = js.noImpl
-    override fun addAll(index: Int, c: Collection<out E>) : Boolean = js.noImpl
+    override fun addAll(index: Int, c: jet.Collection<out E>) : Boolean = js.noImpl
 
     override fun remove(index: Int): E = js.noImpl
 
@@ -73,29 +73,19 @@ public class HashSet<E>(): AbstractCollection<E>(), MutableSet<E> {
 }
 
 library
-public open class HashMap<K, V>() : MutableMap<K, V> {
-    public override fun size() : Int = js.noImpl
-    public override fun isEmpty() : Boolean = js.noImpl
-    public override fun get(key : Any?) : V? = js.noImpl
-    public override fun containsKey(key : Any?) : Boolean = js.noImpl
-    public override fun put(key : K, value : V) : V = js.noImpl
-    public override fun putAll(m : Map<out K, out V>) : Unit = js.noImpl
-    public override fun remove(key : Any?) : V? = js.noImpl
-    public override fun clear() : Unit = js.noImpl
-    public override fun containsValue(value : Any?) : Boolean = js.noImpl
-    public override fun keySet() : Set<K> = js.noImpl
-    public override fun values() : Collection<V> = js.noImpl
-    public override fun entrySet() : Set<MutableMap.MutableEntry<K, V>> = js.noImpl
-}
-
-library
-public open class LinkedList<E>(): AbstractList<E>() {
-    public override fun get(index: Int): E = js.noImpl
-    public override fun set(index: Int, element: E): E = js.noImpl
-    public override fun add(index: Int, element: E): Unit = js.noImpl
-    public fun poll(): E? = js.noImpl
-    public fun peek(): E? = js.noImpl
-    public fun offer(e: E): Boolean = js.noImpl
+public open class HashMap<K, V>(): MutableMap<K, V> {
+    override public fun size(): Int = js.noImpl
+    override public fun isEmpty(): Boolean = js.noImpl
+    override public fun get(key: Any?): V? = js.noImpl
+    override public  fun containsKey(key: Any?): Boolean = js.noImpl
+    override public fun put(key: K, value: V): V = js.noImpl
+    override public fun putAll(m: jet.Map<out K, out V>): Unit = js.noImpl
+    override public fun remove(key: Any?): V? = js.noImpl
+    override public fun clear(): Unit = js.noImpl
+    override public fun containsValue(value: Any?): Boolean = js.noImpl
+    override public fun keySet(): MutableSet<K> = js.noImpl
+    override public fun values(): MutableCollection<V> = js.noImpl
+    override public fun entrySet(): MutableSet<MutableMap.MutableEntry<K, V>> = js.noImpl
 }
 
 library
