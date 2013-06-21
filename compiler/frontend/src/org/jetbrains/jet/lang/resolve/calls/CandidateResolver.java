78cfbcdbc1e2faefec85e06dc8b64964ea89c5f4
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2013-06-21 13:54:42 +0400

    check argument types through complete phase with right data flow info

--- compiler/frontend/src/org/jetbrains/jet/lang/resolve/calls/CandidateResolver.java
+++ compiler/frontend/src/org/jetbrains/jet/lang/resolve/calls/CandidateResolver.java
@@ -355,7 +355,11 @@ public class CandidateResolver {
                         continue;
                     }
                     JetType type = context.trace.get(BindingContext.EXPRESSION_TYPE, expression);
+                    DataFlowInfo dataFlowInfoForValueArgument = resolvedCall.getDataFlowInfoForValueArgument(argument);
                     ResolutionContext<?> newContext = context.replaceExpectedType(expectedType);
+                    if (dataFlowInfoForValueArgument != null) {
+                        newContext = newContext.replaceDataFlowInfo(dataFlowInfoForValueArgument);
+                    }
                     DataFlowUtils.checkType(type, expression, newContext);
                     continue;
                 }
@@ -612,6 +616,7 @@ public class CandidateResolver {
                 if (TypeUtils.dependsOnTypeParameters(expectedType, candidateCall.getCandidateDescriptor().getTypeParameters())) {
                     expectedType = NO_EXPECTED_TYPE;
                 }
+                candidateCall.setDataFlowInfoForArgument(argument, candidateCall.getDataFlowInfo());
                 CallResolutionContext<?> newContext = context.replaceDataFlowInfo(candidateCall.getDataFlowInfo()).replaceBindingTrace(trace)
                         .replaceExpectedType(expectedType);
                 JetTypeInfo typeInfoForCall = argumentTypeResolver.getArgumentTypeInfo(
